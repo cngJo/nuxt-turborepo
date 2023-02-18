5181872c512ff2bb44f011764b794458e39a5266
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2023-02-18 21:25:21 +0100

    chore: migrate packages/ui to vue

---
+++ packages/ui/src/button.vue
@@ -0,0 +1,9 @@
+<template>
+    <button><slot /></button>
+</template>
+
+<style>
+button {
+    color: blue;
+}
+</style>
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,5 @@
+import Button from "./button.vue"
+
+export {
+    Button
+}
